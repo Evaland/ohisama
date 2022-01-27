f467d653dc5a5e27b5cb4617b61a7b0b3999f044
adachi <ne201240@senshu-u.jp>
2022-01-27 01:10:27 +0000

    ohisama8

---
+++ db/seeds/production/items.rb
@@ -0,0 +1,77 @@
+   Item.create(
+      author_category: Category.find_by(category_name: "デイリー食品"),
+      item_name: "牛乳",
+      price: 100,
+      item_quantity: 1000,
+      disable: true
+    )
+    Item.create(
+      author_category: Category.find_by(category_name: "デイリー食品"),
+      item_name: "卵",
+      price: 100,
+      item_quantity: 1000,
+      disable: true
+    )
+    Item.create(
+      author_category: Category.find_by(category_name: "デイリー食品"),
+      item_name: "食パン",
+      price: 100,
+      item_quantity: 1000,
+      disable: true
+    )
+    Item.create(
+      author_category: Category.find_by(category_name: "生鮮"),
+      item_name: "ほうれん草",
+      price: 140,
+      item_quantity: 100,
+      disable: true
+    )
+    Item.create(
+      author_category: Category.find_by(category_name: "生鮮"),
+      item_name: "トマト",
+      price: 160,
+      item_quantity: 100,
+      disable: true
+    )
+    Item.create(
+      author_category: Category.find_by(category_name: "冷凍食品"),
+      item_name: "冷凍餃子",
+      price: 350,
+      item_quantity: 100,
+      disable: true
+    )
+    Item.create(
+      author_category: Category.find_by(category_name: "生鮮"),
+      item_name: "マグロ",
+      price: 400,
+      item_quantity: 100,
+      disable: true
+    )
+    Item.create(
+      author_category: Category.find_by(category_name: "デイリー食品"),
+      item_name: "ヨーグルト",
+      price: 160,
+      item_quantity: 100,
+      disable: true
+    )
+    Item.create(
+      author_category: Category.find_by(category_name: "加工食品"),
+      item_name: "クリームシチュー",
+      price: 320,
+      item_quantity: 100,
+      disable: true
+    )
+    Item.create(
+      author_category: Category.find_by(category_name: "飲料・菓子・アイス"),
+      item_name: "ハイボール",
+      price: 130,
+      item_quantity: 100,
+      disable: true
+    )
+    Item.create(
+      author_category: Category.find_by(category_name: "日用品・化粧品"),
+      item_name: "界面活性剤",
+      price: 520,
+      item_quantity: 100,
+      disable: true
+    )
