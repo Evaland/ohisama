181f1a7a1bbef8818654b032fd35e1c93bb3cdc8
adachi <ne201240@senshu-u.jp>
2022-01-28 08:08:55 +0000

    ohisama13

---
+++ db/seeds/development/regulars.rb
@@ -0,0 +1,30 @@
+Regular.create(
+    member_id: Member.find_by(id: 8).id,
+    regular_quantity: 1,
+    item_id: Item.find_by(item_name: "牛乳").id
+)
+Regular.create(
+    member_id: Member.find_by(id: 8).id,
+    item_id: Item.find_by(item_name: "卵").id,
+    regular_quantity: 1
+)
+Regular.create(
+    member_id: Member.find_by(id: 9).id,
+    regular_quantity: 1,
+    item_id: Item.find_by(item_name: "牛乳").id
+)
+Regular.create(
+    member_id: Member.find_by(id: 9).id,
+    item_id: Item.find_by(item_name: "卵").id,
+    regular_quantity: 1
+)
+Regular.create(
+    member_id: Member.find_by(id: 10).id,
+    regular_quantity: 1,
+    item_id: Item.find_by(item_name: "牛乳").id
+)
+Regular.create(
+    member_id: Member.find_by(id: 10).id,
+    item_id: Item.find_by(item_name: "卵").id,
+    regular_quantity: 0
+)
